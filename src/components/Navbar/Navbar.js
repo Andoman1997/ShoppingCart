3d909899f20417a0977e692d7b05ff6cbde47849
Andranik Manukyan <79224395+Andoman1997@users.noreply.github.com>
2021-12-24 13:06:13 +0400

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = ({ cart }) => {
   return (
     <div className={styles.navbar}>
       <Link to="/">
-        <h2 className={styles.navbar__logo}>Redux Shopping Cart</h2>
+        <h2 className={styles.navbar__logo}>Shopping Cart</h2>
       </Link>
       <Link to="/cart">
         <div className={styles.navbar__cart}>
